b14305d5e58530e825e381fc160ddd467af2acf4
shipengcheng <shipp31@163.com>
2021-11-25 14:40:02 +0800

    hot-fix first commit

--- src/main/java/cn/shipc/git/GitDemo.java
+++ src/main/java/cn/shipc/git/GitDemo.java
@@ -3,5 +3,6 @@ package cn.shipc.git;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("git-----01");
+        System.out.println("hot-fix-----01");
     }
 }
